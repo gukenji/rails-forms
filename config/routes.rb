644210b3bdb50aa7e70a1e4317127f05cb3e5d6e
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2022-06-28 17:48:41 -0300

    tasks completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :edit, :show, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/users/edit/:id', to: 'users#edit'
+  get '/users/:id', to: 'users#show'
+  # get '/users/:id', to: 'users#update'
+  # patch '/users/:id', to: 'users#update'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
